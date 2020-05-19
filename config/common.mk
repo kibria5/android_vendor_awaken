36ced15b1c3ef1c2e8217d569bce154c14b00f92
Jesse Chan <jc@lineageos.org>
2020-05-19 14:01:25 +0800

    vendor: Implement hide gestural navigation hint bar [3/3]
    Change-Id: I5ed5c55ec46c3bb5e91aeb88bd5f06bd4445c2f0
    Signed-off-by: Jesse Chan <jc@lineageos.org>
    
    -----
    
    overlay: Immersive: Import clean gestural overlay
    
    Change-Id: I3d62f787493c8a117373f1dae8366768cee571ce
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>
    
    -----
    
    overlay: Hide navbar properly
    
    Change-Id: I3709e9cec9963538c4aa776807ac5d38db9760fb
    Co-authored-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- config/common.mk
+++ config/common.mk
@@ -236,6 +236,7 @@ PRODUCT_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     CustomPixelLauncherOverlay \
     DocumentsUIOverlay \
+    ImmersiveNavigationOverlay \
     NetworkStackOverlay \
     NexusLauncherResOverlay
 
