c73f3cadd13d209ad7ec8a2e961077a48484571f
Dyneteve <dyneteve@hentaios.com>
2020-12-13 00:57:11 +0900

    config: common: Build the textclassifier models
    Change-Id: I9d7d582ddc427998e68e668cc0375958325d8052

--- config/common.mk
+++ config/common.mk
@@ -115,6 +115,13 @@ PRODUCT_PACKAGES += \
     AwakenWallpaperStub \
     TouchGestures
 
+# TextClassifier
+PRODUCT_PACKAGES += \
+    libtextclassifier_annotator_en_model \
+    libtextclassifier_annotator_universal_model \
+    libtextclassifier_actions_suggestions_universal_model \
+    libtextclassifier_lang_id_model
+
 # Themes
 PRODUCT_PACKAGES += \
     AwakenBlackTheme
